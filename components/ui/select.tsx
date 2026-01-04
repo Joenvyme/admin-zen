810b1146ef8057882eb269cfe08a97789dba769e
weblaw <weblaw@macbookpro.home>
2026-01-04 21:12:47 +0100

    Fix: suppression bordures jaunes dans listes déroulantes (Select Radix UI) - styles agressifs avec !important et suppression border-image

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -27,6 +27,11 @@ const SelectTrigger = React.forwardRef<
       fontSize: '16px',
       WebkitTransform: 'scale(1)',
       transform: 'scale(1)',
+      outline: 'none',
+      WebkitTapHighlightColor: 'transparent',
+      borderColor: '#6B6B6B',
+      WebkitBorderImage: 'none',
+      borderImage: 'none',
     }}
     {...props}
   >
@@ -81,11 +86,19 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-blanc text-noir shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border border-gris-clair bg-blanc text-noir shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         position === "popper" &&
           "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
         className
       )}
+      style={{
+        WebkitAppearance: 'none',
+        MozAppearance: 'none',
+        appearance: 'none',
+        outline: 'none',
+        WebkitTapHighlightColor: 'transparent',
+        borderColor: '#E8E8E8',
+      }}
       position={position}
       {...props}
     >
